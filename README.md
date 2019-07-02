6a6a41562aa6ab811b33240000a378c221df419a
Max Haughton <maxhaton@gmail.com>
2019-07-02 04:27:27 +0100

    Added readmne and logo

---
+++ README.md
@@ -0,0 +1,5 @@
+# MIDIFRAK
+MIDIFRAK is (*will*) be a system for building visual shows to go along with music. 
+
+## Current Status 
+Nothing works at the moment, stay tuned
--- source/app.d
+++ source/app.d
@@ -3,13 +3,14 @@ import vibe.vibe;
 void main()
 {
 	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+	settings.port = 80;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 
 	auto route = new URLRouter; 
 
 	route.get("/", &hello);
 	route.get("/newdesign", &startNew);
+	route.get("/jsontester/:input/:number", &jsonTest);
 	route.get("*", serveStaticFiles("public/"));
 
 
@@ -17,6 +18,16 @@ void main()
 	
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 	runApplication();
+}
+void jsonTest(HTTPServerRequest req, HTTPServerResponse res)
+{
+	import std.conv;
+	struct output {
+		string input;
+		int wah;
+	}
+	logInfo(req.params["input"]);
+	res.writePrettyJsonBody(output(req.params["input"], req.params["number"].to!int));
 }
 void startNew(HTTPServerRequest req, HTTPServerResponse res)
 {
